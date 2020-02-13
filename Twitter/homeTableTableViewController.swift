173a1718564055f4db1f309aed4902e261a904f9
Christian Lay-Geng <35509282+christianlaygeng@users.noreply.github.com>
2020-02-12 22:42:03 -0800

    Fixed the Constraints + Bonus Scrolling User Stories

--- Twitter/homeTableTableViewController.swift
+++ Twitter/homeTableTableViewController.swift
@@ -13,17 +13,49 @@ class homeTableTableViewController: UITableViewController {
     var tweetArray = [NSDictionary]()
     var numberOfTweet: Int!
     
+    let myRefreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweet()
+        loadTweets()
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+        
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
         
     }
     
-    func loadTweet(){
+    @objc func loadTweets(){
        
-    let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-    let myParams = ["count": 10]
+        numberOfTweet = 20
         
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": numberOfTweet]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success:
+            { (tweets: [NSDictionary]) in
+                
+                self.tweetArray.removeAll()
+                
+                for tweet in tweets {
+                    self.tweetArray.append(tweet)
+                }
+                self.tableView.reloadData()
+                self.myRefreshControl.endRefreshing()
+                
+        }, failure: { (Error) in
+            print("Could not retreive tweets!")
+        })
+    }
+
+    func loadMoreTweets(){
+        
+        numberOfTweet = numberOfTweet + 20
+        
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": numberOfTweet]
+    
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success:
             { (tweets: [NSDictionary]) in
                 
@@ -38,7 +70,14 @@ class homeTableTableViewController: UITableViewController {
             print("Could not retreive tweets!")
         })
     }
-
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == tweetArray.count {
+            loadMoreTweets()
+        }
+    }
+    
+    
     @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout()
         self.dismiss(animated: true, completion: nil)
