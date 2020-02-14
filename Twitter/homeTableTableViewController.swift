d39dc834b68bcd21674f7d9e90519665b595e47d
Christian Lay-Geng <35509282+christianlaygeng@users.noreply.github.com>
2020-02-14 01:42:29 -0800

    Other tweet code needed to parse tweet input through Twitter's API

--- Twitter/homeTableTableViewController.swift
+++ Twitter/homeTableTableViewController.swift
@@ -26,6 +26,11 @@ class homeTableTableViewController: UITableViewController {
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
+    
     @objc func loadTweets(){
        
         numberOfTweet = 20
